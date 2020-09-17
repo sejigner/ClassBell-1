4d34030d1e8809a672473af11904881bf2473577
Sejigner <sejigner@gmail.com>
2020-09-18 00:44:34 +0900

    Revert "Revert "- activity_main.xml 레이아웃 수정(padding, layout_margin 활용하여 UI 정리)""
    This reverts commit b5c1619bab2e72bbb40a139f3fc426644c927b9f.

--- app/src/main/java/com/sejin/classbell/MainActivity.java
+++ app/src/main/java/com/sejin/classbell/MainActivity.java
@@ -2,17 +2,27 @@ package com.sejin.classbell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView title;
+    TableLayout table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         title =findViewById(R.id.title);
+        table =findViewById(R.id.tableLayout);
+    }
+
+    public void addClass(View view) {
+        Intent intent = new Intent(MainActivity.this,AddClassActivity.class);
+        startActivity(intent);
     }
 }
